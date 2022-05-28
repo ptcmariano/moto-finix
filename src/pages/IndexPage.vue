5391e322047e601ad398e9eb60034ac780bf1001
Paulo Mariano <paulotiagocm@hotmail.com>
2022-05-28 09:17:30 -0300

    feat: form with notify

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,54 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-form @submit="onSubmit" class="q-gutter-md">
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="hodometro"
+        lazy-rules
+        :rules="[
+          (val) =>
+            (val !== null && val !== '') ||
+            'Please hodometro em numero inteiro',
+        ]"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { ref } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  setup() {
+    const $q = useQuasar();
+
+    const name = ref(null);
+    const age = ref(null);
+    const accept = ref(false);
+
+    return {
+      name,
+      age,
+      accept,
+
+      onSubmit() {
+        $q.notify({
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done",
+          message: "Submitted",
+        });
+      },
+    };
+  },
+});
 </script>
